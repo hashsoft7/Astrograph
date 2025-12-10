98e0062ae94e6b29cf7af5d65bfda2507d0dcf03
Kayden <hollydev0430@gmail.com>
2025-12-10 09:24:00 -0700

    update vite for tauri

--- apps/astrograph-ui/vite.config.ts
+++ apps/astrograph-ui/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isTauri = Boolean(process.env.TAURI_PLATFORM);
+
 export default defineConfig({
+  base: isTauri ? "./" : "/",
   plugins: [react()],
+  clearScreen: false,
+  envPrefix: ["VITE_", "TAURI_"],
   server: {
     port: 4173,
+    strictPort: true,
   },
 });
